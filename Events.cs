a6054cfdeae449a50ffdd6874770bfd3ec775b35
Ads36 <cervenkaadamek@seznam.cz>
2022-07-12 12:03:26 +0200

    slight changes

--- Events.cs
+++ Events.cs
@@ -1,11 +1,35 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace DiscreteSimulationOfDormitory
 {
-	public class Class1
+	abstract class Event : IComparable<Event>
 	{
-		public Class1()
-		{
+        public int Time { get; }
+		public int PrimaryPriority { get; }
+		public int SecondaryPriority { get; }
+		public Event(int time, int primaryPriority, int secondaryPriority)
+        {
+			Time = time;
+			PrimaryPriority = primaryPriority;
+			SecondaryPriority = secondaryPriority;
+        }
+		public int CompareTo(Event other)
+        {
+			if (Time.CompareTo(other.Time) != 0)
+				return Time.CompareTo(other.Time);
+			if (PrimaryPriority.CompareTo(other.PrimaryPriority) == 0)
+            {
+				return SecondaryPriority.CompareTo(other.SecondaryPriority);
+            }
+			return PrimaryPriority.CompareTo(other.PrimaryPriority);
 		}
+		protected abstract void Action(Dormitory dorm);
+		public void Invoke(Dormitory dorm)
+        {
+			Action(dorm);
+        }
 	}
+	//events: opening/closing, changeofvratny, borrowing keys - gym, music room, washing machines, elevators, gym, study rooms, entering and leaving
+
+
 }
---
+++ Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DiscreteSimulationOfDormitory
+{
+	public class Student
+	{
+		public int Number { get; }
+		private static int currentNumber = 1;
+		public Place CurrentPlace;
+		public enum Place
+        {
+			Outside,
+			InRoom,
+			WaitingForElevator,
+			InElevator,
+			InGym,
+			InStudyRoom,
+			WaitingInQueue,
+        }
+		public Student()
+        {
+			Number = currentNumber++;
+        }
+		public Place GetCurrentPlace()
+        {
+			return CurrentPlace;
+        }
+	}
+}
