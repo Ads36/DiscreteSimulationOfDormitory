9f5b61b22fe2c7e3f27f0167970f2ec2acb6679d
Ads36 <cervenkaadamek@seznam.cz>
2022-07-12 12:03:13 +0200

    created elevators

---
+++ Elevators.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DiscreteSimulationOfDormitory
+{
+    public class Elevator
+    {
+
+        public enum State
+        {
+            Up,
+            Down,
+            Still
+        }
+        public State CurrentState;
+        public void Move()
+        {
+            CurrentState = State.Down;
+            if (CurrentState == State.Down)
+            {
+
+            }
+        }
+    }
+}
